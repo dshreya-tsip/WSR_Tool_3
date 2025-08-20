91034616b1fa746faea00e4ac6d65958599cca6c
dshreya-tsip <Shreya.Dhavule@toshiba-tsip.com>
2025-08-20 15:39:35 +0530

    Update test_runner.py

--- test_runner.py
+++ test_runner.py
@@ -6,6 +6,7 @@ import requests
 import subprocess
 import signal
 
+from openpyxl import Workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -190,5 +191,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
